8374013ed190bee2e38b7b33ac70bf299df524b4
Jackson Sugar <33731112+jacksonsugar@users.noreply.github.com>
2021-06-23 10:53:58 -0400

    Add files via upload

---
+++ source/web_iface/View_config.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>View 48 Config</title>
+
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+        background-color:lightskyblue;
+    }
+</style>
+</head>
+<body>
+
+<h2>Minion 48 Config File</h2>
+
+<?php
+
+if ($file = fopen("/home/pi/Desktop/Minion_config.ini", "r")) {
+    while(!feof($file)) {
+        $line = fgets($file);
+        echo $line;
+        echo nl2br("\n");
+    }
+    fclose($file);
+}
+
+?>
+
+<br>
+<br>
+<form action="/index.php" method="post">
+<input type="submit" value="Return">
+</form>
+</body>
+</html>
