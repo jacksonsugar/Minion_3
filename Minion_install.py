2a80bcdf134442600f5d170b651c14c1b7075bb4
Jackson Sugar <33731112+jacksonsugar@users.noreply.github.com>
2021-07-26 11:19:42 -0400

    Update Minion_install.py

--- Minion_install.py
+++ Minion_install.py
@@ -112,7 +112,7 @@ else:
 USBdata = yes_no('Do you wish to configure a USB storage device? (NTFS file system) [Y/N]: ')
 os.system('sudo mkdir /home/pi/Documents/Minion_scripts /home/pi/Documents/Minion_tools')
 # Move the deployment handler so it knows where to look for config file
-os.system('sudo cp source/Data_config.ini source/Minion_DeploymentHandler.py source/Gelcam_DeploymentHandler.py source/Minion_image.py source/Extended_Sampler.py source/Recovery_Sampler_Burn.py source/Recovery_Sampler.py source/OXYBASE_RS232.py source/TempPres.py source/ACC_100Hz.py source/ACC_100Hz_IF.py source/Minion_image_IF.py source/OXYBASE_RS232_IF.py source/minsat.py source/Minsat/SC16IS752GPIO.so source/Iridium_gps.py source/Iridium_data.py source/Temperature_test.py source/Pressure_test.py /home/pi/Documents/Minion_scripts')
+os.system('sudo cp source/Data_config.ini source/Minion_DeploymentHandler.py source/Gelcam_DeploymentHandler.py source/Minion_image.py source/Extended_Sampler.py source/Recovery_Sampler_Burn.py source/Recovery_Sampler.py source/OXYBASE_RS232.py source/TempPres.py source/ACC_100Hz.py source/ACC_100Hz_IF.py source/Minion_image_IF.py source/OXYBASE_RS232_IF.py source/minsat.py source/Minsat/SC16IS752GPIO.so source/Iridium_gps.py source/Iridium_data.py source/Iridium_test.py source/Temperature_test.py source/Pressure_test.py /home/pi/Documents/Minion_scripts')
 
 if USBdata == True:
 
@@ -249,7 +249,7 @@ os.system('sudo cp source/web_iface/sites-enabled/default /etc/nginx/sites-enabl
 
 os.system('sudo /etc/init.d/nginx reload')
 
-os.system('sudo cp source/web_iface/* /var/www/html/')
+os.system('sudo cp source/web_iface/*.py source/web_iface/*.php source/web_iface/*.png /var/www/html/')
 
 os.system('sudo rm -r /var/www/html/index.nginx-debian.html')
 
